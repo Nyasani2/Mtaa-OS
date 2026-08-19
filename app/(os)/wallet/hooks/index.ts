f649dd61baea2434793f5be386ce4551f0684ff3
Kevin Nyasani <nyasanikev@gmail.com>
2026-08-19 16:24:45 +0300

    fix(wallet): add default export to silence Metro route warning

--- app/(os)/wallet/hooks/index.ts
+++ app/(os)/wallet/hooks/index.ts
@@ -3,3 +3,4 @@ export * from './useWalletTaxes';
 export { default as useWalletStore } from './useAgent';
 export { default as useWalletTransactions } from './useAgent';
 export { default as useWallet } from './useAgent';
+export default {};
