c811281941cb86bd66c20918763971a51a30ed57
Kevin Nyasani <nyasanikev@gmail.com>
2026-08-21 12:49:03 +0300

    fix(wallet): domains barrel re-exports real hooks from app/(os)/wallet/hooks (module resolution)

--- domains/wallet/hooks/useWallet.ts
+++ domains/wallet/hooks/useWallet.ts
@@ -1,5 +1,1 @@
-export * from './useWalletAccount';
-export * from './useWalletTaxes';
-export { default as useWalletStore } from './useAgent';
-export { default as useWallet } from './useAgent';
-export { default as useWalletTransactions } from './useAgent';
+export * from '@/app/(os)/wallet/hooks';
